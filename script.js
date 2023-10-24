1f9851a42fc659217cccae52346d691c17836ff2
Moussa Aminata Gueye <kalamouu@gmail.com>
2023-10-24 23:03:53 +0000

    Basic styling and worked on the modal dialog for the "addBook" function

---
+++ script.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector(".modal");
+const addBook = document.querySelector(".addBook")
+
+addBook.addEventListener("click", () => {
+    modal.classList.add("active")
+})
+
