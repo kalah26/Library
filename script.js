e72a714fb82f287a04fb7b43806ab7dc932e9f25
Moussa Aminata Gueye <kalamouu@gmail.com>
2023-11-07 23:29:52 +0000

    improved the book's design and completedd the add new book button/form and gotta resolve the non empty initial library bug

--- script.js
+++ script.js
@@ -18,14 +18,17 @@ class Library {
 
 const library = new Library;
 
-const booksContainer = document.querySelector("books-container");
+const booksContainer = document.querySelector(".books-container");
 
 //user interface
 
 const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".modal")
-const addBook = document.querySelector(".addBook")
-const registerBook = document.querySelector("#registerBook")
+const modal = document.querySelector(".modal");
+const addBook = document.querySelector(".addBook");
+const registerBook = document.querySelector("#registerBook");
+const bookForm = document.querySelector("#bookForm");
+const removeButton = document.querySelector(".removeButton")
+const statusButton = document.querySelector(".statusButton")
 
 //getting input from user
 function getNewBookInfo() {
@@ -41,20 +44,58 @@ function createNewBook(book) {
     const bookTitle = document.createElement('span');
     const bookAuthor = document.createElement('span');
     const bookPages = document.createElement('span');
-    const bookStatus = document.createElement('button')
+    const bookControls = document.createElement('div');
+    const removeButton = document.createElement('button');
+    const statusButton = document.createElement('button');
 
-    bookCard.classList.add("book")
+    bookCard.classList.add("book");
+    bookControls.classList.add("bookControl");
+    removeButton.classList.add("removeButton");
+    statusButton.classList.add("statusButton");
 
     bookTitle.textContent = `Title: ${book.title}`
     bookAuthor.textContent = `Author: ${book.author}`
     bookPages.textContent = `Pages: ${book.pages}`
-    bookStatus.textContent = book.isRead;
+    removeButton.textContent = `Remove`
+    statusButton.textContent = `Read`
+    if (book.isRead) {
+        statusButton.classList.add("read")
+    } else {
+        statusButton.classList.remove("read")
+    }
+
+    bookControls.appendChild(removeButton);
+    bookControls.appendChild(statusButton);
 
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
     bookCard.appendChild(bookPages);
-    bookCard.appendChild(bookStatus);
+    bookCard.appendChild(bookControls);
     booksContainer.appendChild(bookCard);
+}
+
+function resetBookshelf() {
+    booksContainer.innerHTML = '';
+}
+
+function updateBookshelf() {
+    resetBookshelf();
+    for (let book of library.bookShelf) {
+        createNewBook(book);
+    }
+}
+
+function preventDefaults(e) {
+    e.preventDefault()
+}
+
+function addNewBook() {
+    // e.preventDefault();
+    const newBook = getNewBookInfo();
+    createNewBook(newBook);
+    library.addNewBook(newBook)
+    updateBookshelf();
+    removeModal()
 }
 
 function activateModal() {
@@ -71,5 +112,12 @@ const escKeyRemoveModal = (e) => {
     if (e.key === "Escape") removeModal()
 }
 
+statusButton.addEventListener("click", () => {
+    statusButton.classList.toggle("read")
+})
 addBook.addEventListener("click", () => activateModal())
+registerBook.addEventListener("click", ()=> {
+    addNewBook()
+})
+bookForm.onsubmit = addNewBook()
 window.onkeydown = escKeyRemoveModal
